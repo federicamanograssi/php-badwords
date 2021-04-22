8ae9bb8757b03b06056848c887ec5bd54d970f88
Federica <federica.manograssi@gmail.com>
2021-04-22 16:11:44 +0200

    struttura paragrafi
     col mazzo

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $badword=$_GET["badword"];
+
+    $string = "Oggi abbiamo finalmente iniziato quel mazzo di php. La spiegazione è andata liscia come l'olio, ma l'installazione col mazzo che è andata così. C'era anche una simpatica slide con su scritto \"keep calm and var_dump\", ma noi non facevamo altro che pensare \"keep calm sto mazzo\"";
+
+    $newstring = str_replace($badword,'***',$string);
+    // var_dump($array)
+    ?>
+
+    <h1>Paragrafo censored</h1>
+    <p>
+    <?php
+    echo $newstring
+    ?>
+    </p>
+
+    <h1>Paragrafo over 18</h1>
+    <p>
+    <?php
+    echo $string
+    ?>
+    </p>
+
+</body>
+</html>
